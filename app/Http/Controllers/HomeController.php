4134e747c78c138e56a4f42633bfa8b7bd4a6c09
alshoja@gmail.com <alshoja@gmail.com>
2019-07-01 21:21:45 +0530

    #route cleared

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -21,6 +21,8 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('dashboard');
+        $products = Product::where('stock', '>', '0')->with('products', 'images', 'categorie')->orderBy('id', 'desc')->get();
+        return view('welcome', $products);
+        //return view('dashboard');
     }
 }

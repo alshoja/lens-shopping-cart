a1ba8222fa32e16d9c45a77463768625d3460408
alshoja@gmail.com <alshoja@gmail.com>
2019-07-01 21:29:22 +0530

    #route cleared

--- routes/web.php
+++ routes/web.php
@@ -11,7 +11,7 @@
 |
 */
 
-Route::get('/', 'SimpleHome@index')->name('home');
+Route::get('/', 'SimpleHome@index');
 Route::get('product/shop', 'SimpleHome@shop')->name('shop');
 Route::get('product/shop/price', 'SimpleHome@search');
 Route::get('/product/item/{id}', 'SimpleHome@item')->name('item');
